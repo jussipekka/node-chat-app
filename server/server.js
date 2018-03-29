f103bfe0774d5c5a1506be0841881790822fc0d5
jussipekka <jerkkola@gmail.com>
2018-03-29 15:14:39 +0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -17,29 +17,12 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
-
-  socket.emit('newMessage', {
-    from: 'Maisa',
-    text: 'halojata halloo',
-    createdAt: 234
-  })
-
-  // socket.emit('newEmail', {
-  //   from: 'mike@example.com',
-  //   text: 'Hey, what is going on?',
-  //   createAt: 123
-  // });
-
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
-    
-  // });
-
-  // socket.on('disconnect', () => {
-  //   console.log('User disconnected');
-  // });
 });
 
 server.listen(port, () => {
